020c56a276def52cd2d259971bcb5d47d6999381
magocha1 <magocha95.mg@gmail.com>
2018-12-05 22:03:40 -0300

    Actualizacion y nueva pagina

---
+++ validar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iniciar sesion</title>
+</head>
+<body>
+    <section>
+        <article>
+            <?php
+                   if ( ) {
+                       
+                   }     
+            ?>
+        </article>
+    </section>
+    
+</body>
+</html>
