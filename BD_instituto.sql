78d9c95517e9ac0609f48416555dc33e71544b75
baldoor1 <baldooruno@gmail.com>
2018-12-06 17:13:41 -0300

    base de datos modificada

--- BD_instituto.sql
+++ BD_instituto.sql
@@ -3,24 +3,39 @@ use instituto;
 
 SET SQL_SAFE_UPDATES = 0;
 set foreign_key_checks=0;
-SHOW ENGINE INNODB STATUS;
 
-create table alumnos (rut_alum varchar(13) primary key, id_nota int, nom_alum text, nom_carre text, descripcion varchar(500));
+create table alumnos (rut_alum varchar(13) primary key, id_nota int, nom_alum text, id_carre int);
 
-create table profesores (rut_prof varchar(13) primary key, nom_prof text, nom_asig text, nom_carre text);
+create table profesores (rut_prof varchar(13) primary key, nom_prof text, id_asig int, id_carre int);
 
-create table notas (id_nota int primary key, nom_carre text, nom_asig text, nota1 int, nota2 int, nota3 int, nota4 int);
+create table notas (id_nota int primary key, id_carre int, id_asig int, nota1 int, nota2 int, nota3 int, nota4 int);
 
-create table carreras (id_carre int auto_increment primary key, nom_carre text, jefe_carre text, jornada text);
+create table carreras (id_carre int primary key, nom_carre text, jefe_carre text, jornada text, descripcion text);
 
-create table asignaturas (id_asig int auto_increment primary key, nom_asig text, nom_carre text, nom_prof text);
+create table asignaturas (id_asig int primary key, nom_asig text, id_carre int, rut_prof varchar(13));
 
 alter table alumnos 
-add foreign key(id_nota) references notas(id_nota);
+add foreign key(id_nota) references notas(id_nota),
+add foreign key(id_carre) references carreras(id_carre);
 
+alter table profesores 
+add foreign key(id_asig) references asignaturas(id_asig),
+add foreign key(id_carre) references carreras(id_carre);
+
+alter table notas 
+add foreign key(id_carre) references carreras(id_carre),
+add foreign key(id_asig) references asignaturas(id_asig);
+
+alter table asignaturas 
+add foreign key(id_carre) references carreras(id_carre),
+add foreign key(rut_prof) references profesores(rut_prof);
+
+insert into carreras values (11,'analista','matias','diurna');
 insert into notas values (1,'analista programador', 'programacion', 4,5,6,7);
-insert into alumnos values(12345,1,'tomas','analista programador')
 
+insert into alumnos values(12345,1,'tomas',11);
+
+select * from alumnos
 
 select nom_alum, nota1, nota2, nota3, nota4 from alumnos, notas
 where alumnos.id_nota = notas.id_nota and notas.id_nota = 1
